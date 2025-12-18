1962d0369ef03eb194323ef12edab795a09b3c2f
Aeyone <A_Eyone@163.com>
2025-12-18 18:01:38 +0800

    优化写法

--- Problemset/Training/11.30/f.cpp
+++ Problemset/Training/11.30/f.cpp
@@ -9,16 +9,16 @@ using i64 = long long;
 const int MOD = 998244353;
 using i64 = long long;
 
-struct Info{//主要信息
-    i64 mn = INFLL;
-    i64 mn2 = INFLL;
+struct Info{
+    i64 mn = INFLL;     //最小值
+    i64 mn2 = INFLL;    //次小值
     int cnt = 0;
     int len = 0;
 };
 
-struct Lazy{//懒信息
-    i64 add = 0;
-    i64 otherAdd = 0;
+struct Lazy{//由于需要在第二个剪枝的时候只对最小值进行加，而次小值不需要，因此需要两个懒标记
+    i64 add = 0;        //最小值的懒操作
+    i64 otherAdd = 0;   //其他值的懒操作
 };
 
 Info operator+(const Info &l, const Info &r){
@@ -49,28 +49,28 @@ struct SegmentTree{
         init(_n, arr);
     }
 
-    void init(int _n, vector<i64> &arr){
+    void init(int _n, vector<i64> &arr) {
         n = _n;
         info.assign(n << 2, Info(INFLL, INFLL, false));
         lazy.assign(n << 2, Lazy());
-        build(1, 1, n,  arr);
+        build(1, 1, n, arr);
     }
     //对i节点进行+v的懒操作
-    void applyAdd(int p, i64 val1, i64 val2){
+    void applyAdd(int p, i64 val1, i64 val2) {
         info[p].mn += val1;
         info[p].mn2 += (info[p].mn2 == INFLL ? 0 : val2);
         lazy[p].add += val1;
         lazy[p].otherAdd += val2;
     }
 
-    void down(int p){
-        i64 tmp = min(info[2 * p].mn, info[2 * p + 1].mn);
-        if(info[2 * p].mn == tmp){
+    void down(int p) {
+        i64 mn = min(info[2 * p].mn, info[2 * p + 1].mn);
+        if(info[2 * p].mn == mn) {//左儿子存在最小
         	applyAdd(2 * p, lazy[p].add, lazy[p].otherAdd);
         }else{
         	applyAdd(2 * p, lazy[p].otherAdd, lazy[p].otherAdd);
         }
-        if(info[2 * p + 1].mn == tmp){
+        if(info[2 * p + 1].mn == mn) {//右儿子存在最小
         	applyAdd(2 * p + 1, lazy[p].add, lazy[p].otherAdd);
         }else{
         	applyAdd(2 * p + 1, lazy[p].otherAdd, lazy[p].otherAdd);
@@ -78,7 +78,7 @@ struct SegmentTree{
         lazy[p].otherAdd = lazy[p].add = 0;
     }
     //回溯，更新i的值
-    void up(int p){
+    void up(int p) {
         info[p] = info[2 * p] + info[2 * p + 1];
     }
     //构建线段树 O(N)
@@ -106,33 +106,29 @@ struct SegmentTree{
             up(p);//回溯
         }
     }
-    i64 query(int p, int l, int r, int L, int R){
-        i64 res = 0;
-        if(L > r || R < l || info[p].mn > 0){//第一个剪枝：最小值大于0不操作
-            return res;
+    i64 query(int p, int l, int r, int L, int R, int k){
+        if(L > r || R < l || info[p].mn > k){   //第一个剪枝：最小值大于k不操作
+            return 0;
         }
-        if(L <= l && r <= R && info[p].mn2 > 0 ){//第二个剪枝：l~r被ql~qr完全包含时，且次小值大于0，添加懒标记
-        	res += info[p].mn * info[p].cnt;
-        	applyAdd(p, -info[p].mn, 0);
-        }else{
+        i64 res = 0;
+        if(L <= l && r <= R && info[p].mn2 > k){//第二个剪枝：l~r被ql~qr完全包含时，且次小值大于k，只需要对最小值添加懒标记
+        	res += (k - info[p].mn) * info[p].cnt;
+        	applyAdd(p, k - info[p].mn, 0);
+        }else{                                  //暴力下发过程
             int mid = (l + r) / 2;
-            down(p);//l~r不完全被ql~qr包含，懒信息下发
-            res += query(2 * p, l, mid, L, R);
-            res += query(2 * p + 1, mid + 1, r, L, R);
+            down(p);                            
+            res += query(2 * p, l, mid, L, R, k);
+            res += query(2 * p + 1, mid + 1, r, L, R, k);
             up(p);
         }
-        return res;
+        return res;//统计累计上升的总和
     }
     //1-based
     void modify(int L, int R, int val){ 
-        if(L <= R){
-            modify(1, 1, n, L , R , val);
-        }
+        modify(1, 1, n, L , R , val);
     }
-    i64 query(int L, int R){
-        if(L <= R){
-            return query(1, 1, n, L , R );
-        }
+    i64 query(int L, int R, int k){
+        return query(1, 1, n, L, R, k);
     }
 };
 
@@ -151,7 +147,7 @@ void solve() {//吉如一线段树
 		cin >> l >> r >> k;
 		i64 res = 1ll * (r - l + 1) * k;
 		st.modify(l, r, -k);
-		res += st.query(l, r);
+		res -= st.query(l, r, 0);
 		cout << res << '\n';
 	}
 }
--- Problemset/Training/12.16/c.cpp
+++ Problemset/Training/12.16/c.cpp
@@ -26,7 +26,6 @@ void build() {
 		}
 	}
 }
-
 i64 qmi(i64 a, i64 b, i64 p) {
 	i64 res = 1;
 	for( ;b ; b >>= 1, a = (i128)a * a % p) if(b & 1) {
@@ -35,37 +34,38 @@ i64 qmi(i64 a, i64 b, i64 p) {
 	return res;
 }
 
-bool Miller_Rabin(i64 n) { //米勒罗宾判断素数
-    i64 cnt = 0, m = n - 1;
-    if(n == 2 || n == 3) {
+// 米勒-拉宾素性测试
+bool isprime(i64 n) {
+    if(n == 2 || n == 3){
         return true;
     }
-    if(n == 1 || (n % 6 != 1 && n % 6 != 5)) {
+    if(n <= 1 || n % 6 != 1 && n % 6 != 5){
         return false;
     }
-    while(m % 2 == 0) {
-    	m /= 2, cnt ++;
-    }
-    for(int i = 1; i <= 10; i ++) {
-        i64 x = 1ll * rand() * rand() % (n - 1) + 1, res = qmi(x, m, n), lres = res;
-        if(res == 1){
-            continue;
+    static constexpr int A[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };  // 预定义的基数
+    int s = __builtin_ctzll(n - 1);                                 // 计算 n-1 的尾零个数
+    i64 d = (n - 1) >> s;                                           // d = (n-1) / 2^s
+    for(auto a : A) {
+        if(a == n){
+            return true;                // 如果 a 是 n，直接返回 true
         }
-        for(int i = 1; i <= cnt; i ++) {
-            res = (i128)res * res % n;
-            if(res == 1 && lres != n - 1) {
-            	return false;
-            }
-            if(res == 1) {
+        i64 x = qmi(a, d, n);           // 计算 a^d % n
+        if(x == 1 || x == n - 1){
+            continue;                   // 继续测试其他基数
+        }
+        bool ok = false;
+        for(int i = 0; i < s - 1; i ++) {
+            x = (i128)x * x % n;        // x = x^2 % n
+            if (x == n - 1) {
+                ok = true;              // 找到 n-1，证明 n 是素数
                 break;
             }
-            lres = res;
         }
-        if(res != 1){
-            return false;
+        if(!ok){
+            return false;               // 如果没有找到，n 不是素数
         }
     }
-    return true;
+    return true;                        // 所有基数都通过，n 是素数
 }
 
 void solve() {
@@ -80,7 +80,7 @@ void solve() {
 		}
 	}
 	for(i64 i = L + 1; i <= R; i ++) {
-		ans += Miller_Rabin(i);
+		ans += isprime(i);
 	}
 	cout << ans << '\n';
 }
